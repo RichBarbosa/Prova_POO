a96a683e7864b93cad5b8e7701f90c08a5417e1e
RichBarbosa <70115573+RichBarbosa@users.noreply.github.com>
2020-11-25 14:07:53 -0300

    Correção getList
    Correção getList

--- ProvaPOO/src/java/aula/Disciplina.java
+++ ProvaPOO/src/java/aula/Disciplina.java
@@ -14,10 +14,10 @@ import java.util.ArrayList;
 public class Disciplina {
     private String nome;   
     private String ementa;   
-    private String ciclo;   
+    private int ciclo;   
     private float nota;
 
-    public Disciplina(String nome, String ementa, String ciclo) {
+    public Disciplina(String nome, String ementa, int ciclo) {
         this.nome = nome;
         this.ementa = ementa;
         this.ciclo = ciclo;
@@ -39,11 +39,11 @@ public class Disciplina {
         this.ementa = ementa;
     }
 
-    public String getCiclo() {
+    public int getCiclo() {
         return ciclo;
     }
 
-    public void setCiclo(String ciclo) {
+    public void setCiclo(int ciclo) {
         this.ciclo = ciclo;
     }
 
@@ -56,13 +56,13 @@ public class Disciplina {
     }
     public static ArrayList<Disciplina> getList(){
         ArrayList<Disciplina> disciplina = new ArrayList();
-        disciplina.add(new Disciplina("programação orientada a objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programação estruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo, agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens de programação. Aplicação e uso das estruturas fundamentais da orientação a objetos.", "4"));
-        disciplina.add(new Disciplina("Banco de dados", "Conceitos de Base de Dados. Modelos conceituais de informações. Modelos de Dados: Relacional,  Redes e Hierárquicos. Modelagem de dados - conceitual, lógica e física. Teoria relacional: dependências funcionais e multivaloradas, formas normais. Restrições de integridade e de segurança em Banco de Dados Relacional. Sistemas Gerenciadores de Banco de Dados – objetivo e funções. Linguagens de declaração e de manipulação de dados.", "4"));
-        disciplina.add(new Disciplina("Sistemas Operacionais 2", "Apresentação de um sistema operacional específico utilizado em ambiente corporativo. Requisitos de hardware para instalação do sistema. Processo de instalação, personalização, operação, administração e segurança sobre o sistema operacional focado.  Elaboração de projetos de seleção e implantação de um sistema operacional.", "4"));
-        disciplina.add(new Disciplina("inglês", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", "4"));
-        disciplina.add(new Disciplina("metodologia da pesquisa cintifico-tecnologica", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", "4"));
-        disciplina.add(new Disciplina("engenharia de software 3", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadas no desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise e Projetos. Formas de representação. O processo de desenvolvimento. Mapeamento para implementação. Integração do sistema. Testes: planejamento e tipos. Manutenção. Documentação.", "4"));
-        disciplina.add(new Disciplina("Linguagem de programação 4", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmi­cas e interativas. Definição de layouts e formatação em geral. Adição de algorítmos usando laços, matrizes, datas, funções e condições. Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração com Banco de Dados. Exercícios práticos com projeto de criação de sites.", "4"));
+        disciplina.add(new Disciplina("programação orientada a objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programação estruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo, agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens de programação. Aplicação e uso das estruturas fundamentais da orientação a objetos.", 4));
+        disciplina.add(new Disciplina("Banco de dados", "Conceitos de Base de Dados. Modelos conceituais de informações. Modelos de Dados: Relacional,  Redes e Hierárquicos. Modelagem de dados - conceitual, lógica e física. Teoria relacional: dependências funcionais e multivaloradas, formas normais. Restrições de integridade e de segurança em Banco de Dados Relacional. Sistemas Gerenciadores de Banco de Dados – objetivo e funções. Linguagens de declaração e de manipulação de dados.", 4));
+        disciplina.add(new Disciplina("Sistemas Operacionais 2", "Apresentação de um sistema operacional específico utilizado em ambiente corporativo. Requisitos de hardware para instalação do sistema. Processo de instalação, personalização, operação, administração e segurança sobre o sistema operacional focado.  Elaboração de projetos de seleção e implantação de um sistema operacional.", 4));
+        disciplina.add(new Disciplina("inglês", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", 4));
+        disciplina.add(new Disciplina("metodologia da pesquisa cintifico-tecnologica", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", 4));
+        disciplina.add(new Disciplina("engenharia de software 3", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadas no desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise e Projetos. Formas de representação. O processo de desenvolvimento. Mapeamento para implementação. Integração do sistema. Testes: planejamento e tipos. Manutenção. Documentação.", 4));
+        disciplina.add(new Disciplina("Linguagem de programação 4", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmi­cas e interativas. Definição de layouts e formatação em geral. Adição de algorítmos usando laços, matrizes, datas, funções e condições. Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração com Banco de Dados. Exercícios práticos com projeto de criação de sites.", 4));
 
         
         return disciplina;
--- ProvaPOO/web/disciplinas.jsp
+++ ProvaPOO/web/disciplinas.jsp
@@ -10,22 +10,20 @@
 <%
     ArrayList<Disciplina> disciplina = (ArrayList)application.getAttribute("disciplina");
     if(disciplina == null){
-        disciplina = new ArrayList();
-        disciplina.add(new Disciplina("programação orientada a objetos", "Conceitos e evolução da tecnologia de orientação a objetos. Limitações e diferenças entre o paradigma da programação estruturada em relação à orientação a objetos. Conceito de objeto, classe, métodos, atributos, herança, polimorfismo, agregação, associação, dependência, encapsulamento, mensagem e suas respectivas notações na linguagem padrão de representação da orientação a objetos. Implementação de algoritmos orientado a objetos utilizando linguagens de programação. Aplicação e uso das estruturas fundamentais da orientação a objetos.", "4"));
-        disciplina.add(new Disciplina("Banco de dados", "Conceitos de Base de Dados. Modelos conceituais de informações. Modelos de Dados: Relacional,  Redes e Hierárquicos. Modelagem de dados - conceitual, lógica e física. Teoria relacional: dependências funcionais e multivaloradas, formas normais. Restrições de integridade e de segurança em Banco de Dados Relacional. Sistemas Gerenciadores de Banco de Dados – objetivo e funções. Linguagens de declaração e de manipulação de dados.", "4"));
-        disciplina.add(new Disciplina("Sistemas Operacionais 2", "Apresentação de um sistema operacional específico utilizado em ambiente corporativo. Requisitos de hardware para instalação do sistema. Processo de instalação, personalização, operação, administração e segurança sobre o sistema operacional focado.  Elaboração de projetos de seleção e implantação de um sistema operacional.", "4"));
-        disciplina.add(new Disciplina("inglês", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", "4"));
-        disciplina.add(new Disciplina("metodologia da pesquisa cintifico-tecnologica", "Consolidação da compreensão e produção oral e escrita por meio da integração das habilidades lingüístico-comunicativas desenvolvidas na disciplina Inglês 3. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos sócio-culturais da língua inglesa.", "4"));
-        disciplina.add(new Disciplina("engenharia de software 3", "Conceitos, evolução e importância de arquitetura de software. Padrões de Arquitetura. Padrões de Distribuição. Camadas no desenvolvimento de software. Tipos de Arquitetura de Software. Visões na arquitetura de software. Modelo de Análise e Projetos. Formas de representação. O processo de desenvolvimento. Mapeamento para implementação. Integração do sistema. Testes: planejamento e tipos. Manutenção. Documentação.", "4"));
-        disciplina.add(new Disciplina("Linguagem de programação 4", "Comandos de linguagens usadas na construção e estruturação de sites para a Web, com páginas dinâmi­cas e interativas. Definição de layouts e formatação em geral. Adição de algorítmos usando laços, matrizes, datas, funções e condições. Introdução a Orientação a Objetos utilizando objetos, métodos e propriedades. Integração com Banco de Dados. Exercícios práticos com projeto de criação de sites.", "4"));
+        disciplina = Disciplina.getList();
         application.setAttribute("disciplina", disciplina);
     
     }
+    String ExceptionMessage = null;
     if(request.getParameter("def") != null){
+        try{
         int i = Integer.parseInt(request.getParameter("i"));
         float nota = Float.parseFloat(request.getParameter("n"));
-        Disciplina disc = disciplina.get(i);
-        disc.setNota(nota);
+        disciplina.get(i).setNota(nota);
+        response.sendRedirect(request.getRequestURI());
+        }catch(Exception ex){
+           ExceptionMessage = ex.getLocalizedMessage();
+        }
     }
 
 %>
@@ -38,24 +36,28 @@
     </head>
     <body>
         <h1>Avaliação POO</h1>
+        <%if(ExceptionMessage != null){%>
+        <div style="color: red"> Erro ao exibir nota: <%=ExceptionMessage%></div>
+        <%}%>
         <table border="1" >
         <tr>
             <th>Indice</th>
             <th>Disciplina</th>
             <th>Ementa</th>
             <th>Ciclo</th>
+            <th>nota</th>
+            <th>Alterar nota</th>
         </tr>
         <%for(int i = 0; i < disciplina.size(); i++){%>
         <tr>
             <td><%=i%></td>
-            <%Disciplina d = disciplina.get(i); %>
-            <td><%=d.getNome() %></td>
-            <td><%=d.getEmenta() %></td>
-            <td><%=d.getCiclo() %></td>
-            <td><%=d.getNota() %></td>
+            <td><%=disciplina.get(i).getNome() %></td>
+            <td><%=disciplina.get(i).getEmenta() %></td>
+            <td><%=disciplina.get(i).getCiclo() %></td>
+            <td><%=disciplina.get(i).getNota() %></td>
             <td>
                 <form>
-                    <input type="number" name="n">
+                    <input type="number" name="n" step="0.1" min="1" max="10">
                     <input type="hidden" name="i" value="<%= i %>">
                     <input type="submit" name="def" value="definir">
                 </form>
